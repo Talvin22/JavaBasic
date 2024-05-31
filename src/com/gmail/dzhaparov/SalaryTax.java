fa6b533596353d0ff7c352dcc828a98725d123cd
Talvin22 <talvindzaparov1@gmail.com>
2024-05-31 10:40:06 +0200

    adding the tax manager

---
+++ src/com/gmail/dzhaparov/SalaryTax.java
@@ -0,0 +1,41 @@
+package com.gmail.dzhaparov;
+
+import java.util.Scanner;
+
+public class SalaryTax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int salary = scanner.nextInt();
+        float taxPercent = 0;
+        double tax = 0;
+        double result = 0;
+
+        scanner.close();
+
+        if (salary >= 0 && salary <= 10_000){
+            taxPercent = 2.5f;
+            tax = (taxPercent / 100) * salary;
+            result = salary - tax;
+        }
+
+        else if (salary > 10_000 && salary <= 25_000){
+            taxPercent = 4.3f;
+            tax = (taxPercent / 100) * salary;
+            result = salary - tax;
+        }
+
+        else if (salary > 25_000){
+            taxPercent = 6.7f;
+            tax = (taxPercent / 100) * salary;
+            result = salary - tax;
+        }
+
+        System.out.printf("Salary before taxes: %d \n"+
+                "Tax was: %.1f (%.2f) \n" +
+                "Salary after taxes: %.2f", salary, taxPercent, tax, result);
+
+
+
+    }
+}
