c6a76d1c79523ea8fc928446547af78beae93e00
Talvin22 <talvindzaparov1@gmail.com>
2024-05-31 11:07:38 +0200

    fix tax task

--- src/com/gmail/dzhaparov/SalaryTax.java
+++ src/com/gmail/dzhaparov/SalaryTax.java
@@ -9,32 +9,28 @@ public class SalaryTax {
         int salary = scanner.nextInt();
         float taxPercent = 0;
         double tax = 0;
-        double result = 0;
+        double netSalary = 0;
 
         scanner.close();
 
-        if (salary >= 0 && salary <= 10_000){
+        if (salary >= 0 && salary <= 10_000) {
             taxPercent = 2.5f;
             tax = (taxPercent / 100) * salary;
-            result = salary - tax;
-        }
-
-        else if (salary > 10_000 && salary <= 25_000){
+            netSalary = salary - tax;
+        } else if (salary > 10_000 && salary <= 25_000) {
             taxPercent = 4.3f;
             tax = (taxPercent / 100) * salary;
-            result = salary - tax;
-        }
-
-        else if (salary > 25_000){
+            netSalary = salary - tax;
+        } else if (salary > 25_000) {
             taxPercent = 6.7f;
             tax = (taxPercent / 100) * salary;
-            result = salary - tax;
+            netSalary = salary - tax;
         }
 
-        System.out.printf("Salary before taxes: %d \n"+
-                "Tax was: %.1f (%.2f) \n" +
-                "Salary after taxes: %.2f", salary, taxPercent, tax, result);
 
+        System.out.printf("Salary before taxes: %d \n" +
+                "Tax was: %.1f (%.2f) \n" +
+                "Salary after taxes: %.2f", salary, taxPercent, tax, netSalary);
 
 
     }
