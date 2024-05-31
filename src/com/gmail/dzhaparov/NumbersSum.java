013474d91261be57959da0537fadf122835e6673
Talvin22 <talvindzaparov1@gmail.com>
2024-05-31 11:15:19 +0200

    adding the NumbersSum class

--- src/com/gmail/dzhaparov/NumbersSum.java
+++ src/com/gmail/dzhaparov/NumbersSum.java
@@ -1,2 +1,22 @@
-package com.gmail.dzhaparov;public class NumbersSum {
+package com.gmail.dzhaparov;
+
+import java.util.Scanner;
+
+public class NumbersSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int limit = scanner.nextInt();
+        int sum = 0;
+
+        scanner.close();
+
+        for (int i = 1; i <= limit; i++) {
+            sum += i;
+            System.out.printf("%d) Num is %d, sum is %d \n", i, i, sum);
+
+        }
+
+
+    }
 }
